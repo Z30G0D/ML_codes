87857cbaceffd709111c163c5bbc8b918999845a
Z30G0D <zeogod@gmail.com>
2017-11-28 13:20:39 +0200

    test

--- AndrewNGML/Exercise3/Exercise_3.py
+++ AndrewNGML/Exercise3/Exercise_3.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.io import loadmat
+from scipy.optimize import minimize
 
 data = loadmat("ex3data1.mat")
 
@@ -41,16 +42,36 @@ def gradients(X, y, theta, lamb):
     z = X * theta.T
     # error vector
     error = sigmoid(z) - y
-    # calculating first term with *no* regularization to avoid penalizing all parameters
+    # calculating first term(intercept parameter) with *no* regularization to avoid penalizing all parameters
     first_term = np.multiply(error, X[:, 0])
     grads[0] = np.sum(first_term) / m
     for k in range(param):
         all_terms = np.multiply(error, X[:, k])
-        if (k==0):
+        if k == 0:
             break
         else:
             grads[k] = (np.sum(all_terms) / m) + ((lamb / m) * theta[:, k])
     return grads
 
 
+def one_vs_all(X, y, num_labels, learning_rate):
+    rows = X.shape[0]
+    params = X.shape[1]
 
+    # k X (n + 1) array for the parameters of each of the k classifiers
+    all_theta = np.zeros((num_labels, params + 1))
+
+    # insert a column of ones at the beginning for the intercept term
+    X = np.insert(X, 0, values=np.ones(rows), axis=1)
+
+    # labels are 1-indexed instead of 0-indexed
+    for i in range(1, num_labels + 1):
+        theta = np.zeros(params + 1)
+        y_i = np.array([1 if label == i else 0 for label in y])
+        y_i = np.reshape(y_i, (rows, 1))
+
+        # minimize the objective function
+        fmin = minimize(fun=cost, x0=theta, args=(X, y_i, learning_rate), method='TNC', jac=gradient)
+        all_theta[i - 1, :] = fmin.x
+
+    return all_theta
