0d251d009e6f55e664bdae15469fc52537a205bd
Z30G0D <zeogod@gmail.com>
2017-12-09 15:46:41 +0200

    test

---
+++ AndrewNGML/Exercise6/Exercise_6_gaussian.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pylab
+from sklearn import svm
+from scipy.io import loadmat
+
+mydata2 = loadmat('Data/ex6data2.mat')
