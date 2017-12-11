4c1cbf6586898e8443325a2acc97b141a2383851
Z30G0D <zeogod@gmail.com>
2017-12-11 19:24:27 +0200

    test

--- AndrewNGML/Exercise6/Exercise_6_gaussian.py
+++ AndrewNGML/Exercise6/Exercise_6_gaussian.py
@@ -5,5 +5,19 @@ import seaborn as sb
 import pylab
 from sklearn import svm
 from scipy.io import loadmat
-
+import numpy as np
 mydata2 = loadmat('Data/ex6data2.mat')
+
+def gaussian(xi, xj, sigma):
+    """This function calculates the similarity between two examples (each one contains two features in this example"""
+    result = np.exp(-(np.sum(np.square(xi - xj)) / (2 * np.square(sigma))))
+
+    return result
+
+
+first = np.array([1, 2])
+second = np.array([1, 5])
+
+print (gaussian(first, second, 1))
+
+
