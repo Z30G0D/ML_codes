1e42cb5a487730e887ff179d48d4595b39c91f76
Z30G0D <zeogod@gmail.com>
2017-12-20 17:16:36 +0200

    Exercise7

--- Kaggle/Titanic/Titanic.py
+++ Kaggle/Titanic/Titanic.py
@@ -4,8 +4,12 @@ import pandas as pd
 
 
 data = pd.read_csv('train.csv')
-survived = data['Survived']
-
-print (survived)
+#
+survived = data[data['Survived'] == 1]['Sex'].value_counts()
+not_survived = data[data['Survived'] == 0]['Sex'].value_counts()
+df = pd.DataFrame([survived, not_survived])
+df.index = ['Survived', 'Not survived']
+df.plot(kind='barh', stacked=True, figsize=[15, 9])
+plt.show()
 #data.plot.bar(x='Sex', y='Survived')
-#plt.show()
+plt.show()
